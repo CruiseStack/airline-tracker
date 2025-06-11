f9f59b969952d4a9af752ef437e36c64828b8166
Osmangulen <OGULEN21@ku.edu.tr>
2025-06-11 21:21:24 +0300

    Revert "Update models.py"
    This reverts commit 6f2acc08b604e6f1a67b6270d065243da323b46d.

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -16,26 +16,3 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
-class Ticket(models.Model):
-    ticket_number = models.AutoField(primary_key=True)
-    PNR_number = models.CharField(max_length=20, unique=True)
-    ticketing_timestamp = models.DateTimeField(default=timezone.now)
-    checkin_status = models.BooleanField(default=False)
-    seat_number = models.CharField(max_length=5)
-    gate_number = models.CharField(max_length=5)
-
-    passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE, related_name="tickets")
-    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, related_name="tickets")
-    flight_instance = models.ForeignKey(FlightInstance, on_delete=models.CASCADE, related_name="tickets")
-
-    FLIGHT_CLASS_CHOICES = [
-        ('ECONOMY', 'Economy'),
-        ('BUSINESS', 'Business'),
-    ]
-    class_type = models.CharField(max_length=10, choices=FLIGHT_CLASS_CHOICES)
-    carry_on = models.BooleanField(default=True)
-    baggage = models.IntegerField(default=0)
-    extra_baggage = models.IntegerField(default=0)
-
-    def __str__(self):
-        return f"Ticket #{self.ticket_number} - PNR {self.PNR_number}"
